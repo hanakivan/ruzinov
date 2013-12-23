40a5b2ece77fccaad17786f9668a8ee372229307
Ivan Hanák <hanakivan@gmail.com>
2013-12-23 22:36:20 +0100

    Stránka je momentálne hotová. Môžeme ju odovzdať klientovi.

---
+++ js/ruzinov.js
@@ -0,0 +1,59 @@
+jQuery(document).ready(function($){
+	
+	/**
+	 * Show slider navigation
+	 * If the browser has wide viewport, the chevrons are visible, othervise they are hidden
+	 * However, when we will make this slide dynamic, 
+	 */
+	$('#slider-navigation').removeAttr('hidden');
+	
+	$(window).on('load resize', function(){
+		if($(window).outerWidth() > 1180)
+			$('#slider').removeClass('narrow');
+		else
+			$('#slider').addClass('narrow');
+	});
+		
+	/**
+	 * Alter static height of the slider when the window changed its dimensions
+	 */
+	$(window).on('load resize', function(){
+		$('#slider-content').css('height', parseInt($('#slider img:visible:first').outerHeight()));
+	});
+	
+	/**
+	 * Fix, if there is not placeholder attribute support
+	 */
+	if (document.createElement("input").placeholder == undefined) {
+		var text;
+		var diese;//yeah, german
+		$("input[placeholder]:not([type='password']), textarea").each(function(){
+			diese = $(this);
+			diese.addClass('no-placeholder');
+			text = diese.attr('placeholder');
+			if(diese.val() == '')
+				diese.val(text);
+		});
+	}
+	
+	$(".no-placeholder").focus(function(){
+		diese = $(this);
+		diese.attr("data-temp", diese.val());
+		diese.val('');
+	}).blur(function(){
+		diese = $(this);
+		if(diese.val() == '')
+		{
+			diese.val(diese.attr("data-temp"));
+			diese.removeAttr("data-temp");
+		}
+	});
+	
+	/**
+	 * Open pop-up window when sharing
+	 */
+	$('#share-buttons a').click(function(e){
+		e.preventDefault();
+		window.open($(this).attr("href"),'','width=600,height=300,left=200,top=200');
+	});
+});
